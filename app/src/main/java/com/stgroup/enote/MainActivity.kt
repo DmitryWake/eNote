f29d21f4126bf12f015e3730fc8f16ee7a10d81d
ArthurChains <arthur.salio@gmail.com>
2020-10-10 21:36:19 +0300

    ENOTE-7 Я сделалъ

--- app/src/main/java/com/stgroup/enote/MainActivity.kt
+++ app/src/main/java/com/stgroup/enote/MainActivity.kt
@@ -7,6 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFields()
+    }
+
+    private fun initFields() {
+
+    }
+
+    private fun initFunc() {
+
     }
 }
 
