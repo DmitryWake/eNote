b7f20f01021989b52f721ca86fb7db221766a05a
ArthurChains <arthur.salio@gmail.com>
2020-10-22 17:32:59 +0300

    done restartActivity()

--- app/src/main/java/com/stgroup/enote/utilities/funs.kt
+++ app/src/main/java/com/stgroup/enote/utilities/funs.kt
@@ -1,7 +1,9 @@
 package com.stgroup.enote.utilities
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.stgroup.enote.MainActivity
 import com.stgroup.enote.R
 
 fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = true) {
@@ -20,3 +22,11 @@ fun replaceFragment(fragment: Fragment, addToBackStack: Boolean = true) {
 fun Context.showToast(message: CharSequence) =
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
+
+// Restart activity function
+fun restartActivity(){
+    val intent = Intent(APP_ACTIVITY, MainActivity::class.java)
+    APP_ACTIVITY.startActivity(intent)
+
+    APP_ACTIVITY.finish()
+}
