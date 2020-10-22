402e048ccd198b113172db9e0cfb392c5ead2043
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-10-22 18:14:46 +0300

    ENOTE-24 Creating RubbishFragment.kt and layout

--- app/src/main/java/com/stgroup/enote/screens/RubbishFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/RubbishFragment.kt
@@ -3,10 +3,11 @@ package com.stgroup.enote.screens
 import androidx.fragment.app.Fragment
 import com.stgroup.enote.utilities.APP_ACTIVITY
 
-class NoteCategoryFragment: Fragment() {
+class RubbishFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.mDrawer.enableDrawer()
     }
+
 }
