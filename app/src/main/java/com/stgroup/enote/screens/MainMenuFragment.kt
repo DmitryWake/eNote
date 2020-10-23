6c12784c308480c57e93e1eb3fa6fa563ddbffe4
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-10-23 19:21:19 +0300

    ENOTE-41 Create base design. Note auto open

--- app/src/main/java/com/stgroup/enote/screens/MainMenuFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/MainMenuFragment.kt
@@ -1,12 +1,15 @@
 package com.stgroup.enote.screens
 
 import androidx.fragment.app.Fragment
+import com.stgroup.enote.R
 import com.stgroup.enote.utilities.APP_ACTIVITY
+import com.stgroup.enote.utilities.replaceFragment
 
-class MainMenuFragment : Fragment() {
+class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.mDrawer.enableDrawer()
+        replaceFragment(NoteFragment())
     }
 }
--- app/src/main/java/com/stgroup/enote/screens/NoteFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/NoteFragment.kt
@@ -1,6 +1,7 @@
 package com.stgroup.enote.screens
 
 import androidx.fragment.app.Fragment
+import com.stgroup.enote.R
 
-class NoteFragment : Fragment() {
+class NoteFragment : Fragment(R.layout.fragment_note) {
 }
