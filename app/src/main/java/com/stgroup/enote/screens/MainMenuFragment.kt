c90c58e2f35515b25978537a3e5243a5b1519513
Dmitry_Wake <nikolaevskiy2001@gmail.com>
2020-11-01 22:11:09 +0300

    ENOTE-32 Первычная версия category_item.xml

--- app/src/main/java/com/stgroup/enote/screens/MainMenuFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/MainMenuFragment.kt
@@ -2,18 +2,15 @@ package com.stgroup.enote.screens
 
 import androidx.fragment.app.Fragment
 import com.stgroup.enote.R
-import com.stgroup.enote.models.NoteModel
-import com.stgroup.enote.screens.note_screen.NoteFragment
 import com.stgroup.enote.utilities.APP_ACTIVITY
-import com.stgroup.enote.utilities.replaceFragment
 
 class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.mDrawer.enableDrawer()
-        // Временно
-        val note = NoteModel(
+        // Временно для открытия заметки
+        /*val note = NoteModel(
             "New note",
             "null",
             "Test",
@@ -22,6 +19,6 @@ class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
             "null",
             "NightSky"
         )
-        replaceFragment(NoteFragment(note))
+        replaceFragment(NoteFragment(note))*/
     }
 }
