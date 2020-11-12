b049d39264d92195c8248b016ba5cbe03f178335
HeavenlyRaven <36207275+HeavenlyRaven@users.noreply.github.com>
2020-11-12 04:42:44 +0300

    ENOTE-63 Добавлен макет fragment_auth_phone.xml

--- app/src/main/java/com/stgroup/enote/screens/SettingsFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/SettingsFragment.kt
@@ -1,13 +1,16 @@
 package com.stgroup.enote.screens
 
 import androidx.fragment.app.Fragment
+import com.stgroup.enote.R
 import com.stgroup.enote.utilities.APP_ACTIVITY
 
 
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
 
-class SettingsFragment: Fragment() {
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.mDrawer.enableDrawer()
     }
+
+
 }
