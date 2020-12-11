00b47f597d4252e40eb0f5b7957f1af343652f34
HeavenlyRaven <36207275+HeavenlyRaven@users.noreply.github.com>
2020-12-11 17:58:56 +0300

    ENOTE-80 Реализовано диалоговое окно создания категории

--- app/src/main/java/com/stgroup/enote/screens/main_menu_screen/MainMenuFragment.kt
+++ app/src/main/java/com/stgroup/enote/screens/main_menu_screen/MainMenuFragment.kt
@@ -1,5 +1,10 @@
 package com.stgroup.enote.screens.main_menu_screen
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.NumberPicker
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
@@ -82,9 +87,39 @@ class MainMenuFragment : Fragment(R.layout.fragment_main_menu) {
     }
 
     private fun addCategory() {
-        categoryList.add(CategoryModel(UUID.randomUUID().toString(), "New category", 3))
-        categoryList.sortBy { it.priority }
-        mAdapter.updateData(categoryList)
+
+        var categoryName = ""
+        var priority = 3
+        val dialogView = LayoutInflater.from(APP_ACTIVITY).inflate(R.layout.dialog_create_category, null)
+        dialogView.findViewById<EditText>(R.id.input_name).addTextChangedListener(object :
+            TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                categoryName = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        with(dialogView.findViewById<NumberPicker>(R.id.input_priority)){
+            maxValue = 20
+            minValue = 0
+            value = priority
+            setOnValueChangedListener { _, _, newVal ->
+                priority = newVal
+            }
+        }
+
+        AlertDialog.Builder(APP_ACTIVITY)
+            .setTitle(R.string.create_category_title)
+            .setView(dialogView)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                categoryList.add(CategoryModel(UUID.randomUUID().toString(), categoryName, priority))
+                categoryList.sortBy { it.priority }
+                mAdapter.updateData(categoryList)
+            }
+            .show()
     }
 
     private fun initCategories() {
